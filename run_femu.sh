5a79af0dd34f54e8371687da57de52c7c61dccb0
parkjongheum <whdgmawkd@naver.com>
2023-07-10 15:03:09 -0700

    Add FEMU Run script

---
+++ run_femu.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+set -e
+
+ROOT=$(pwd)
+
+if [ ! -d ./femu/build-femu ]
+then
+    echo "Installing FEMU..."
+    # Install FEMU
+    # Download and Extract
+    wget -q --show-progress "https://github.com/vtess/FEMU/archive/refs/tags/femu-v8.0.0.tar.gz" -O "femu.tar.gz"
+    mkdir -p femu
+    tar zxf femu.tar.gz -C femu --strip-components=1
+
+    # Build FEMU
+    cd femu
+    mkdir build-femu
+    cd build-femu
+    cp ../femu-scripts/femu-copy-scripts.sh .
+    ./femu-copy-scripts.sh .
+    # update OSIMGF
+    sed -i 's/u20s.qcow2/ubuntu2204.qcow2/g' run-zns.sh
+    sudo ./pkgdep.sh
+    ./femu-compile.sh
+fi
+
+echo $ROOT
+cd $ROOT/femu/build-femu
+./run-zns.sh
