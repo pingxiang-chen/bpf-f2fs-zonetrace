a5c7a19b6505785bcb39569cba16bc2f4ddbcb10
ChangHoon Sung <changhoon.dev@gmail.com>
2023-07-11 11:55:54 -0700

    Fix ring buffer overhead and chores

--- src/bpf/f2fs.bpf.c
+++ src/bpf/f2fs.bpf.c
@@ -1,64 +1,46 @@
 // SPDX-License-Identifier: GPL-2.0 OR BSD-3-Clause
 /* Copyright (c) 2021 Sartura */
 #include "f2fs.h"
+
+#include <bpf/bpf_core_read.h>
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_tracing.h>
-#include <bpf/bpf_core_read.h>
 
 char LICENSE[] SEC("license") = "Dual BSD/GPL";
 
 /* BPF ringbuf map */
 struct {
-	__uint(type, BPF_MAP_TYPE_RINGBUF);
-	__uint(max_entries, 8 * 4096 * 1024 /* 32 MB */);
+    __uint(type, BPF_MAP_TYPE_RINGBUF);
+    __uint(max_entries, 256 * 1024 /* 256KB */);
 } rb SEC(".maps");
 
 struct event {
-	int segno;
-	unsigned char cur_valid_map[65];
+    int segno;
+    unsigned char cur_valid_map[65];
 };
 
 SEC("fexit/update_sit_entry")
-int BPF_PROG(update_sit_entry, struct f2fs_sb_info *sbi, block_t blkaddr)
-{
-	struct event *e;
-	e = bpf_ringbuf_reserve(&rb, sizeof(*e), 0);
-	if (!e)
-		return 0;
+int BPF_PROG(update_sit_entry, struct f2fs_sb_info *sbi, block_t blkaddr) {
+    struct event *e;
+    e = bpf_ringbuf_reserve(&rb, sizeof(*e), 0);
+    if (!e) {
+        bpf_printk("kprobe:update_sit_entry: ringbuf_reserve failed\n");
+        return 0;
+    }
 
-	struct seg_entry *ses = BPF_CORE_READ(sbi, sm_info, sit_info, sentries);
+    struct seg_entry *ses = BPF_CORE_READ(sbi, sm_info, sit_info, sentries);
 
-	struct f2fs_sm_info *SM_I = BPF_CORE_READ(sbi, sm_info);
+    struct f2fs_sm_info *SM_I = BPF_CORE_READ(sbi, sm_info);
 
-	int segno = ((blkaddr <= 0) ?
-			     (4294967295U) :
-			     (((((blkaddr) -
-				 (SM_I != 0 ? BPF_CORE_READ(SM_I, seg0_blkaddr) :
-					      BPF_CORE_READ(sbi, raw_super, segment0_blkaddr))) >>
-				BPF_CORE_READ(sbi, log_blocks_per_seg))) -
-			      BPF_CORE_READ(SM_I, free_info, start_segno)));
+    // The BTF doesn't record #define macros, so we unfold the fs/f2fs/segment.h GET_SEGNO macro here
+    int segno = ((blkaddr <= 0) ? (4294967295U) : (((((blkaddr) - (SM_I != 0 ? BPF_CORE_READ(SM_I, seg0_blkaddr) : BPF_CORE_READ(sbi, raw_super, segment0_blkaddr))) >> BPF_CORE_READ(sbi, log_blocks_per_seg))) - BPF_CORE_READ(SM_I, free_info, start_segno)));
 
-	struct seg_entry *se = ses + segno;
-	unsigned char *bitmap = BPF_CORE_READ(se, cur_valid_map);
+    struct seg_entry *se = ses + segno;
+    unsigned char *bitmap = BPF_CORE_READ(se, cur_valid_map);
 
-	e->segno = segno;
-	bpf_core_read(&e->cur_valid_map, sizeof(e->cur_valid_map), (void *)bitmap);
+    e->segno = segno;
+    bpf_core_read(&e->cur_valid_map, sizeof(e->cur_valid_map), (void *)bitmap);
 
-	bpf_ringbuf_submit(e, 0);
-
-	// bpf_printk("kprobe:update_sit_entry ENTRY pid = %d, segno = %d %02x", pid, segno, bitmap);
-
-	// bpf_loop(nr_loops, callback, &se, 0);
-
-	return 0;
+    bpf_ringbuf_submit(e, 0);
+    return 0;
 }
-
-// SEC("kretprobe/update_sit_entry")
-// int BPF_KRETPROBE(update_sit_entry)
-// {
-// 	pid_t pid;
-
-// 	pid = bpf_get_current_pid_tgid() >> 32;
-// 	bpf_printk("KPROBE EXIT: pid = %d, ret = %ld\n", pid, ret);
-// 	return 0;
-// }
--- src/bpf/f2fs.c
+++ src/bpf/f2fs.c
@@ -2,127 +2,130 @@
 /* Copyright (c) 2021 Sartura
  * Based on minimal.c by Facebook */
 
-#include <stdio.h>
-#include <unistd.h>
-#include <signal.h>
-#include <string.h>
-#include <errno.h>
-#include <sys/resource.h>
 #include <bpf/libbpf.h>
+#include <errno.h>
+#include <signal.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/resource.h>
+#include <unistd.h>
+
 #include "f2fs.skel.h"
 
 int nr_zone;
 int nr_op_zone;
 int zone_size;
+int zone_blocks;
+int zone_cap_blocks;
 
 struct event {
-	int segno;
-	unsigned char cur_valid_map[65];
+    int segno;
+    unsigned char cur_valid_map[65];
 };
 
-static int libbpf_print_fn(enum libbpf_print_level level, const char *format, va_list args)
-{
-	return vfprintf(stderr, format, args);
+static int libbpf_print_fn(enum libbpf_print_level level, const char *format, va_list args) {
+    return vfprintf(stderr, format, args);
 }
 
 static volatile sig_atomic_t stop;
 
-static void sig_int(int signo)
-{
-	stop = 1;
+static void sig_int(int signo) {
+    stop = 1;
 }
 
-void bump_memlock_rlimit(void)
-{
-	struct rlimit rlim_new = {
-		.rlim_cur = RLIM_INFINITY,
-		.rlim_max = RLIM_INFINITY,
-	};
+void bump_memlock_rlimit(void) {
+    struct rlimit rlim_new = {
+        .rlim_cur = RLIM_INFINITY,
+        .rlim_max = RLIM_INFINITY,
+    };
 
-	if (setrlimit(RLIMIT_MEMLOCK, &rlim_new)) {
-		fprintf(stderr, "Failed to increase RLIMIT_MEMLOCK limit!\n");
-		exit(1);
-	}
+    if (setrlimit(RLIMIT_MEMLOCK, &rlim_new)) {
+        fprintf(stderr, "Failed to increase RLIMIT_MEMLOCK limit!\n");
+        exit(1);
+    }
 }
 
-int handle_event(void *ctx, void *data, size_t data_sz)
-{
-	const struct event *e = data;
+int handle_event(void *ctx, void *data, size_t data_sz) {
+    const struct event *e = data;
 
-	const unsigned int segment_size = 2; // 2 MiB
-	unsigned int seg_per_zone = zone_size / segment_size;
-	unsigned int cur_zone = e->segno / seg_per_zone;
-	printf("update_sit_entry segno: %d cur_zone:%d\n", e->segno, cur_zone);
-	for (int i = 0; i < 64; i++) {
-		printf("%02x ", e->cur_valid_map[i]);
-	}
-	printf("\n");
-	return 0;
+    const unsigned int segment_size = 2;  // 2 MiB
+    unsigned int seg_per_zone = zone_size / segment_size;
+    unsigned int cur_zone = e->segno / seg_per_zone;
+
+    printf("update_sit_entry segno: %d cur_zone:%d\n", e->segno % 1024, cur_zone);
+    fflush(stdout);
+    write(1, e->cur_valid_map, 64);
+    printf("\n");
+    return 0;
 }
 
-int main(int argc, char **argv)
-{
-	if (argc < 4) {
-		printf("Usage: sudo %s <nr_zone> <nr_op_zone> <zone_size(MiB)>\n", argv[0]);
-		return 1;
-	}
+int main(int argc, char **argv) {
+    if (argc < 4) {
+        printf("Usage: sudo %s <nr_zone> <nr_op_zone> <zone_size(MiB)>\n", argv[0]);
+        return 1;
+    }
+    struct ring_buffer *rb = NULL;
+    struct f2fs_bpf *skel;
+    int err;
 
-	nr_zone = atoi(argv[1]);
-	nr_op_zone = atoi(argv[2]);
-	zone_size = atoi(argv[3]);
+    nr_zone = atoi(argv[1]);
+    nr_op_zone = atoi(argv[2]);
+    zone_size = atoi(argv[3]);
 
-	struct ring_buffer *rb = NULL;
-	struct f2fs_bpf *skel;
-	int err;
+    zone_blocks = 524288;
+    zone_cap_blocks = 275712;
 
-	/* Set up libbpf errors and debug info callback */
-	libbpf_set_print(libbpf_print_fn);
+    printf("info: total_zone=%d zone_blocks=%d zone_cap_blocks=%d\n", nr_zone, zone_blocks, zone_cap_blocks);
+    fflush(stdout);
 
-	/* Bump RLIMIT_MEMLOCK to create BPF maps */
-	bump_memlock_rlimit();
+    /* Set up libbpf errors and debug info callback */
+    libbpf_set_print(libbpf_print_fn);
 
-	if (signal(SIGINT, sig_int) == SIG_ERR) {
-		fprintf(stderr, "can't set signal handler: %s\n", strerror(errno));
-		goto cleanup;
-	}
+    /* Bump RLIMIT_MEMLOCK to create BPF maps */
+    bump_memlock_rlimit();
 
-	/* Open load and verify BPF application */
-	skel = f2fs_bpf__open_and_load();
-	if (!skel) {
-		fprintf(stderr, "Failed to open BPF skeleton\n");
-		return 1;
-	}
+    if (signal(SIGINT, sig_int) == SIG_ERR) {
+        fprintf(stderr, "can't set signal handler: %s\n", strerror(errno));
+        goto cleanup;
+    }
 
-	/* Attach tracepoint handler */
-	err = f2fs_bpf__attach(skel);
-	if (err) {
-		fprintf(stderr, "Failed to attach BPF skeleton\n");
-		goto cleanup;
-	}
+    /* Open load and verify BPF application */
+    skel = f2fs_bpf__open_and_load();
+    if (!skel) {
+        fprintf(stderr, "Failed to open BPF skeleton\n");
+        return 1;
+    }
 
-	/* Set up ring buffer polling */
-	rb = ring_buffer__new(bpf_map__fd(skel->maps.rb), handle_event, NULL, NULL);
-	if (!rb) {
-		err = -1;
-		fprintf(stderr, "Failed to create ring buffer\n");
-		goto cleanup;
-	}
+    /* Attach tracepoint handler */
+    err = f2fs_bpf__attach(skel);
+    if (err) {
+        fprintf(stderr, "Failed to attach BPF skeleton\n");
+        goto cleanup;
+    }
 
-	while (!stop) {
-		err = ring_buffer__poll(rb, 0 /* timeout, ms */);
-		/* Ctrl-C will cause -EINTR */
-		if (err == -EINTR) {
-			err = 0;
-			break;
-		}
-		if (err < 0) {
-			printf("Error polling ring buffer: %d\n", err);
-			break;
-		}
-	}
+    /* Set up ring buffer */
+    rb = ring_buffer__new(bpf_map__fd(skel->maps.rb), handle_event, NULL, NULL);
+    if (!rb) {
+        err = -1;
+        fprintf(stderr, "Failed to create ring buffer\n");
+        goto cleanup;
+    }
+
+    while (!stop) {
+        err = ring_buffer__consume(rb);
+        /* Ctrl-C will cause -EINTR */
+        if (err == -EINTR) {
+            err = 0;
+            break;
+        }
+        if (err < 0) {
+            printf("Error consuming ring buffer: %d\n", err);
+            break;
+        }
+    }
 
 cleanup:
-	ring_buffer__free(rb);
-	f2fs_bpf__destroy(skel);
-	return -err;
+    ring_buffer__free(rb);
+    f2fs_bpf__destroy(skel);
+    return -err;
 }
