a904b1f26f789cd2eedad349d68abf5e0bff1102
parkjongheum <whdgmawkd@naver.com>
2023-07-13 13:49:48 -0700

    Update mount_and_tracing: fix syntax error, read blkaddr from file

--- scripts/mount_and_tracing.sh
+++ scripts/mount_and_tracing.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+BLKADDR_FILE=/tmp/f2fs_start_blkaddr.txt
+
 ROOT=$(pwd)
 if [ $(basename $PWD) = "scripts" ]
 then
@@ -9,7 +11,13 @@ then
 fi
 cd $ROOT
 
-# run mount_f2fs.sh and get zoned start_blkaddr
-START_BLKADDR = $(sudo ./scripts/mount_f2fs.sh | awk '/start_blkaddr/ {print $2}')
+if [ ! -f $BLKADDR_FILE ]
+then
+    echo "start_blkaddr is not found. formatting f2fs..."
+    # run mount_f2fs.sh and get zoned start_blkaddr
+    START_BLKADDR=$(sudo ./scripts/mount_f2fs.sh | awk '/start_blkaddr/ {print $2}')
+    echo $START_BLKADDR > $BLKADDR_FILE
+fi
 
+START_BLKADDR=$(cat $START_BLKADDR)
 sudo ./src/bpf/f2fszonetracer nvme0n1 $START_BLKADDR | ./viewer/viewer
