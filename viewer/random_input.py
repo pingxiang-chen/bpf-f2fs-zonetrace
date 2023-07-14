2ab10297b5cd86060167651b3e0f708a3a8708aa
2minchul <2_minchul@naver.com>
2023-07-13 20:52:03 -0700

    Fix zone display size issue

--- viewer/random_input.py
+++ viewer/random_input.py
@@ -11,6 +11,7 @@ def read_buf(buf):
 
 if __name__ == '__main__':
     sys.stdout.buffer.write(b'info: total_zone=905 zone_blocks=524288\n')
+    sys.stdout.flush()
     buf = io.BytesIO()
     seg_types = [random.randint(0, 5) for i in range(905)]
     while True:
