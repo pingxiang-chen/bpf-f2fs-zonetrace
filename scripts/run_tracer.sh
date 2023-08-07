71aa8b69f3c4c2452742ec863e1febc2fe5f5a6b
ChangHoon Sung <changhoon.dev@gmail.com>
2023-08-07 16:55:46 -0700

    Fix typo

--- scripts/run_tracer.sh
+++ scripts/run_tracer.sh
@@ -28,4 +28,4 @@ START_BLKADDR=$(cat $BLKADDR_FILE | awk '{print $2}')
 ZNS_DEVICE=$(cat $BLKADDR_FILE | awk '{print $3}')
 REGULAR_DEVICE=$(cat $BLKADDR_FILE | awk '{print $4}')
 
-./src/bpf/f2fszonetracer $ZNS_DEVICE $REGULAR_DEIVICE $MAIN_BLKADDR $START_BLKADDR | ./viewer/viewer
+./src/bpf/f2fszonetracer $ZNS_DEVICE $REGULAR_DEVICE $MAIN_BLKADDR $START_BLKADDR | ./viewer/viewer
