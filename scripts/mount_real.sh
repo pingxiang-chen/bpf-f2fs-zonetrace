b9edfee5448e9378328964b93a0c05ffdf912f3a
Ping-Xiang Chen <p.x.chen@uci.edu>
2023-09-13 10:03:41 -0700

    move fio jobs files to one single folder

--- scripts/mount_real.sh
+++ scripts/mount_real.sh
@@ -2,10 +2,10 @@
 
 ZNS_DEVICE=nvme3n1
 REGULAR_DEVICE=nvme4n1p1
-
-MOUNT_POINT=/mnt/f2fs
+ENABLE_GC_MERGE=$1
 
 OVERPROVISIONING_SECTIONS=3
+MOUNT_POINT=/mnt/f2fs
 
 BLKADDR_FILE=$HOME/.config/zonetracer/f2fs_blkaddr.txt
 MKFS_LOG_FILE=$HOME/.config/zonetracer/mkfs.f2fs
@@ -16,29 +16,31 @@ then
     exit
 fi
 
-# 1. Create blkaddr file for recording main_blkaddr and start_blkaddr when mkfs.f2fs
+if [ "$#" -ne 1 ]; then
+    echo "Usage: ./mount_zns.sh [enable gc_merge]"
+	exit -1
+fi
+
 if [ ! -d $(dirname $BLKADDR_FILE) ]
 then
     mkdir -p $(dirname $BLKADDR_FILE)
 fi
 
-# 2. Create mount point
 mkdir -p $MOUNT_POINT
 umount -q $MOUNT_POINT
 
-# 3. Format f2fs
-mkfs.f2fs -d 1 -o $OVERPROVISIONING_SECTIONS -m -f -c /dev/$ZNS_DEVICE /dev/$REGULAR_DEVICE | tee $MKFS_LOG_FILE
+sudo bash -c "echo mq-deadline > /sys/block/$ZNS_DEVICE/queue/scheduler"
 
-if [ $? -ne 0 ]
-then
-    echo "Format f2fs failed"
-    exit 1
+if [ $ENABLE_GC_MERGE -eq 1 ]; then
+	echo "Enable GC Merge in f2fs"
+	mkfs.f2fs -d 1 -o $OVERPROVISIONING_SECTIONS,gc_merge -m -f -c /dev/$ZNS_DEVICE /dev/$REGULAR_DEVICE | tee $MKFS_LOG_FILE
+else
+	echo "No  GC Merge in f2fs"
+	mkfs.f2fs -d 1 -o $OVERPROVISIONING_SECTIONS -m -f -c /dev/$ZNS_DEVICE /dev/$REGULAR_DEVICE | tee $MKFS_LOG_FILE
 fi
 
-# 4. Record main_blkaddr and start_blkaddr
 MAIN_BLKADDR=$(cat $MKFS_LOG_FILE | awk '/main_blkaddr/{print $NF}')
 START_BLKADDR=$(cat $MKFS_LOG_FILE | awk '/start_blkaddr/ {print $2}')
 echo "$MAIN_BLKADDR $START_BLKADDR $ZNS_DEVICE $REGULAR_DEVICE" > $BLKADDR_FILE
 
-# 5. Mount f2fs
 mount -t f2fs /dev/$REGULAR_DEVICE $MOUNT_POINT
