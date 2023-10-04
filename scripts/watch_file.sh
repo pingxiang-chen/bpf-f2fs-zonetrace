a0ec1d8b47d2ea679a75385ee0d9f9f0dc60b89b
Ping-Xiang Chen <p.x.chen@uci.edu>
2023-10-03 17:17:41 -0700

    Add fiemap and watch file scripts

--- scripts/watch_file.sh
+++ scripts/watch_file.sh
@@ -1,6 +1,28 @@
 #!/bin/bash
+FILE=$1
 
-MOUNT_POINT=/mnt/f2fs
-FILE=test.0.0
+if [ "$EUID" -ne 0 ]
+then
+    echo "Please run as root"
+    exit
+fi
 
-[ -f $MOUNT_POINT/$FILE ] && watch -n 1 ../../fiemap/fiemap $MOUNT_POINT/$FILE
+if [ "$#" -ne 1 ]; then
+    echo "Usage: ./mount_zns.sh [filename]"
+	exit -1
+fi
+
+if [ ! -f $FILE ]; then
+    echo "file does not exist"
+    exit -1
+fi
+
+ROOT=$(pwd)
+if [ $(basename $PWD) = "scripts" ]
+then
+    ROOT=$(dirname $PWD)
+fi
+cd $ROOT
+
+./fiemap/fiemap $FILE
+[ -f $FILE ] && watch -n 1 ../fiemap/fiemap $FILE
